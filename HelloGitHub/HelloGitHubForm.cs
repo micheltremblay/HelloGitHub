153c471a2e30835dc6b366bdb0c1b1d3c3910ddd
Michel.Tremblay <Michel.Tremblay@CCNB2004897>
2023-09-19 09:02:41 -0300

    Add project files.

---
+++ HelloGitHub/HelloGitHubForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloGitHub
+{
+    public partial class HelloGitHubForm : Form
+    {
+        public HelloGitHubForm()
+        {
+            InitializeComponent();
+        }
+
+        private void helloButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello GitHub");
+        }
+    }
+}
